28d904cce8eea246321098f4925d1b13cdd42f59
Juan Martinez <jmshebar259@gmail.com>
2018-07-12 11:49:56 -0500

    Include timed guide.

--- Version2/VirtualGym_HolotoolKit/Assets/Scripts/CollisionBound.cs
+++ Version2/VirtualGym_HolotoolKit/Assets/Scripts/CollisionBound.cs
@@ -15,6 +15,8 @@ namespace HoloToolkit.Unity.InputModule
         public int CarryCount { get; protected set; }
         public Text counter;
         private GameObject InitialSphere { get; set; }
+        public bool StartTraining { get; private set; }
+        Component[] selectionLights = null;
 
         void Start()
         {
@@ -22,12 +24,15 @@ namespace HoloToolkit.Unity.InputModule
             CarryCount = 0;
             FinalCount = 0;
             TrainingRange = true;
-
+            StartTraining = false;
         }
 
         void OnTriggerEnter(Collider boxCollider)
         {
-            if (boxCollider.attachedRigidbody.gameObject.name == HandDraggable.Instance.HostTransform.gameObject.transform.GetChild(0).name)
+            Color b0b0 = new Color32(11, 218, 3, 255);
+            StartTraining = true;
+            selectionLights = boxCollider.attachedRigidbody.gameObject.transform.GetChild(0).GetComponentsInChildren(typeof(Transform));
+            if (boxCollider.attachedRigidbody.gameObject.name == HandDraggable.Instance.HostTransform.gameObject.name)
             {
                 if (transform.parent.name == "Sphere")
                 {
@@ -36,19 +41,31 @@ namespace HoloToolkit.Unity.InputModule
                     if (transform.name == "leftStart" || transform.name == "rightStart")
                     {
                         CollisionBound.Instance.InitialSphere = transform.gameObject;
-                        CollisionBound.Instance.InitialSphere.GetComponent<Renderer>().material.color = Color.yellow;
-                    }
-
-
+                        //CollisionBound.Instance.InitialSphere.GetComponent<Renderer>().material.color = Color.yellow;
+                       
+                        for(int lhtC = 1; lhtC < selectionLights.Length; lhtC++)
+                        {
+                            selectionLights[lhtC].gameObject.GetComponent<Renderer>().material.color = b0b0;
+                        }
+                     
+                        //Debug.Log(boxCollider.attachedRigidbody.gameObject.transform.GetChild(0).GetComponentInChildren(typeof);
+                    }                    
                     if (transform.name == "leftFinal" || transform.name == "rightFinal")
                     {
+                        
                         if (CollisionBound.Instance.CarryCount == 1)
                         {
                             CollisionBound.Instance.FinalCount += CollisionBound.Instance.CarryCount;
                             counter.text = "Repetitions: " + CollisionBound.Instance.FinalCount.ToString();
                             CollisionBound.Instance.CarryCount--;
                             if (CollisionBound.Instance.InitialSphere != null)
+                            {
                                 CollisionBound.Instance.InitialSphere.GetComponent<Renderer>().material.color = Color.green;
+                                for (int lhtC = 1; lhtC < selectionLights.Length; lhtC++)
+                                {
+                                    selectionLights[lhtC].gameObject.GetComponent<Renderer>().material.color = Color.red;
+                                }
+                            }
                         }
                     }
                 }
@@ -66,8 +83,8 @@ namespace HoloToolkit.Unity.InputModule
 
         void OnTriggerExit(Collider boxCollider)
         {
-            
-            if (boxCollider.attachedRigidbody.gameObject.name == HandDraggable.Instance.HostTransform.gameObject.transform.GetChild(0).name) { 
+            selectionLights = boxCollider.attachedRigidbody.gameObject.transform.GetChild(0).GetComponentsInChildren(typeof(Transform));
+            if (boxCollider.attachedRigidbody.gameObject.name == HandDraggable.Instance.HostTransform.gameObject.name) { 
                 if (transform.parent.name == "Plane")
                 {
                     transform.GetComponent<Renderer>().material.color = Color.red;
@@ -77,6 +94,10 @@ namespace HoloToolkit.Unity.InputModule
                         if (CollisionBound.Instance.InitialSphere != null)
                         {
                             CollisionBound.Instance.InitialSphere.GetComponent<Renderer>().material.color = Color.red;
+                            for (int lhtC = 1; lhtC < selectionLights.Length; lhtC++)
+                            {
+                                selectionLights[lhtC].gameObject.GetComponent<Renderer>().material.color = Color.red;
+                            }
                             transform.GetComponent<Renderer>().material.color = Color.red;
                         }
                         CollisionBound.Instance.CarryCount--;
@@ -85,7 +106,9 @@ namespace HoloToolkit.Unity.InputModule
                 counter.text = "Repetitions: " + CollisionBound.Instance.FinalCount.ToString();
                 if (TrainingRange && (transform.name == "rightStart" || transform.name == "leftStart"))
                 {
+                   
                     CollisionBound.Instance.CarryCount++;
+                    
                     //CollisionBound.Instance.CarryCount = (int)Mathf.Pow(CollisionBound.Instance.CarryCount, 0);
                 }
             }            
---
+++ Version2/VirtualGym_HolotoolKit/Assets/Scripts/PathFollower.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//https://forum.unity.com/threads/move-gameobject-along-a-given-path.455195/
+public class PathFollower : MonoBehaviour {
+    Node[] PathNode;
+    public GameObject Player;
+    public float MoveSpeedForward;
+    public float MoveSpeedBackward;
+    private float MoveSpeed;
+    float Timer;
+    int CurrentNode;
+    static Vector3 CurrentPositionHolder;
+    Vector3 StartPosition;
+    bool ForwardDir = true;
+    int Count = 0;
+	// Use this for initialization
+
+	void Start () {
+        PathNode = GetComponentsInChildren<Node>();
+        CheckNode();
+	}
+	
+    void CheckNode()
+    {
+        Timer = 0;
+        StartPosition = Player.transform.position;
+        CurrentPositionHolder = PathNode[CurrentNode].transform.position;
+    }
+
+    void Forward()
+    {
+        CurrentNode++;
+        MoveSpeed = MoveSpeedForward;
+        CheckNode();
+        
+    }
+
+    void Backward()
+    {
+        CurrentNode--;
+        MoveSpeed = MoveSpeedBackward;
+        CheckNode();
+    }
+
+    void SwitchDir(ref bool Direction)
+    {
+        Direction = !Direction;
+    }
+
+    
+	// Update is called once per frame
+	void Update () {
+        if (HoloToolkit.Unity.InputModule.CollisionBound.Instance.StartTraining)
+        {
+            Timer += Time.deltaTime * MoveSpeed;
+        
+            if(Player.transform.position != CurrentPositionHolder)
+            {
+                Player.transform.position = Vector3.Lerp(StartPosition, CurrentPositionHolder, Timer);
+            }
+        
+            else
+            {
+                if ((PathNode.Length-1 == CurrentNode || CurrentNode == 0) && Count != 0)
+                {
+                    SwitchDir(ref ForwardDir);
+                }
+            
+               if(ForwardDir)
+                    Forward();
+                else
+                    Backward();
+            }
+            Count++;
+        }
+	}
+}
